4e2d7fab0c79d30414386820b45dc4494f56f12c
ks99999 <107827650+ks99999@users.noreply.github.com>
2022-10-06 20:15:15 +0500

    Create nicehive-setup.sh

---
+++ nicehive-setup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+URL=https://raw.githubusercontent.com/ks99999/nicehive/main
+wget --no-cache hello "$URL/nicehive.sh" -O /hive/sbin/nicehive.sh
+chmod a+rx /hive/sbin/nicehive.sh
+echo "*/10 * * * * /hive/sbin/nicehive.sh >> /tmp/nicehive.log" >> /hive/etc/crontab.root
+echo "1 0 * * 1 rm /tmp/nicehive.log" >> /hive/etc/crontab.root
+sync  
